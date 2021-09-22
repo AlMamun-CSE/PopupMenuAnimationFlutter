a969bc7ec87dac2f6285a522615f6ef6c7a7d513
AlMamun-CSE <almamuncse2020@gmail.com>
2021-09-22 23:59:18 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_popupmenu/screens/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/" : (BuildContext context)=> HomePage(),
+      },
+    );
+  }
+}
+
+
