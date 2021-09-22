5ac3b6d0e17d6d2ad58949bf50425c647d81e3fa
AlMamun-CSE <almamuncse2020@gmail.com>
2021-09-23 00:29:23 +0600

    added popupmenu and divider

---
+++ lib/screens/settingspage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Settings'),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+        ),
+      );
+}
---
+++ lib/screens/share_page.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class SharePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Share'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+      );
+}
